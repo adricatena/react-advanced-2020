9d421dd65c4f22c50b7f258de1bceeec129c0c05
Adriano <57508409+adricatena@users.noreply.github.com>
2021-06-24 13:09:53 -0300

    comienzo de operador ternario 6.02.50

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 //import Setup from './tutorial/1-useState/setup/1-error-example'
 //import Setup from './tutorial/1-useState/setup/2-useState-basics'
 //import Setup from './tutorial/1-useState/setup/3-useState-array'
@@ -7,15 +7,15 @@ import React from 'react'
 //import Setup from './tutorial/2-useEffect/setup/1-useEffect-basics'
 //import Setup from './tutorial/2-useEffect/setup/2-useEffect-cleanup'
 //import Setup from './tutorial/2-useEffect/setup/3-useEffect-fetch-data'
-import Setup from './tutorial/3-conditional-rendering/setup/1-multiple-returns'
-
+// import Setup from './tutorial/3-conditional-rendering/setup/1-multiple-returns'
+import Setup from './tutorial/3-conditional-rendering/setup/2-short-circuit'
 
 function App() {
   return (
-    <div className='container'>
-      <Setup ></Setup>
+    <div className="container">
+      <Setup></Setup>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,12 +1,19 @@
-import React, { useState } from 'react';
+import React, { Fragment, useState } from "react";
 // short-circuit evaluation
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+  const [text, setText] = useState("");
+  const firstValue = text || "hello world";
+  const secondValue = text && "hello world";
+  console.log(`firstValue: ${firstValue}, secondValue: ${secondValue}`);
 
-  return <h2>short circuit</h2>;
+  return <Fragment>
+    <h1>{text || "juancito"} </h1>
+    <h1>{text && "hello world"} </h1>
+    {text && <h3>hello world 3</h3>}
+    {!text && <h3>hello world 4</h3>}
+  </Fragment>;
 };
 
 export default ShortCircuit;
