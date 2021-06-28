bc49822855df96d6decb975509860249cc7bfbd7
Adriano <57508409+adricatena@users.noreply.github.com>
2021-06-28 16:28:15 -0300

    inicio form basics 6.20.15 (proyecto 7 inclusive)

--- src/tutorial/3-conditional-rendering/setup/3-show-hide.js
+++ src/tutorial/3-conditional-rendering/setup/3-show-hide.js
@@ -1,7 +1,37 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Fragment } from "react";
 
 const ShowHide = () => {
-  return <h2>show/hide</h2>;
+  const [show, setShow] = useState(false);
+
+  return (
+    <Fragment>
+      <button className="btn" onClick={() => setShow(!show)}>
+        Show/hide
+      </button>
+      {show && <Item />}
+    </Fragment>
+  );
+};
+
+const Item = () => {
+  // valor de inicio para el hook
+  const [size, setSize] = useState(window.innerWidth);
+  // fcion que se ejecuta cada vez que se cambia el ancho
+  const checkSize = () => {
+    setSize(window.innerWidth);
+  };
+  useEffect(() => {
+    window.addEventListener("resize", checkSize);
+    return () => {
+      window.removeEventListener("resize", checkSize);
+    };
+  }, []);
+  return (
+    <div style={{ marginTop: "2rem" }}>
+      <h1>Window</h1>
+      <h2>size: {size} px</h2>
+    </div>
+  );
 };
 
 export default ShowHide;
