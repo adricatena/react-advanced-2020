2231bed2e3fe4414d5c95f9cadc4e0faed11e916
Adriano <57508409+adricatena@users.noreply.github.com>
2021-07-08 15:51:58 -0300

    fin de prop drilling 8.14.40

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React from "react";
-//import Setup from './tutorial/1-useState/setup/1-error-example'
-//import Setup from './tutorial/1-useState/setup/2-useState-basics'
-//import Setup from './tutorial/1-useState/setup/3-useState-array'
-//import Setup from './tutorial/1-useState/setup/4-useState-object'
-//import Setup from './tutorial/1-useState/setup/5-useState-counter'
-//import Setup from './tutorial/2-useEffect/setup/1-useEffect-basics'
-//import Setup from './tutorial/2-useEffect/setup/2-useEffect-cleanup'
-//import Setup from './tutorial/2-useEffect/setup/3-useEffect-fetch-data'
+// import Setup from './tutorial/1-useState/setup/1-error-example'
+// import Setup from './tutorial/1-useState/setup/2-useState-basics'
+// import Setup from './tutorial/1-useState/setup/3-useState-array'
+// import Setup from './tutorial/1-useState/setup/4-useState-object'
+// import Setup from './tutorial/1-useState/setup/5-useState-counter'
+// import Setup from './tutorial/2-useEffect/setup/1-useEffect-basics'
+// import Setup from './tutorial/2-useEffect/setup/2-useEffect-cleanup'
+// import Setup from './tutorial/2-useEffect/setup/3-useEffect-fetch-data'
 // import Setup from './tutorial/3-conditional-rendering/setup/1-multiple-returns'
-//import Setup from './tutorial/3-conditional-rendering/setup/2-short-circuit'
-//import Setup from './tutorial/3-conditional-rendering/setup/3-show-hide'
-//import Setup from "./tutorial/4-forms/setup/1-controlled-inputs";
+// import Setup from './tutorial/3-conditional-rendering/setup/2-short-circuit'
+// import Setup from './tutorial/3-conditional-rendering/setup/3-show-hide'
+// import Setup from "./tutorial/4-forms/setup/1-controlled-inputs";
 // import Setup from "./tutorial/4-forms/setup/2-multiple-inputs";
 // import Setup from "./tutorial/5-useRef/setup/1-useRef-basics";
-import Setup from "./tutorial/6-useReducer/setup";
+// import Setup from "./tutorial/6-useReducer/setup";
+import Setup from "./tutorial/7-prop-drilling/setup/1-prop-drilling";
 
 function App() {
   return (
